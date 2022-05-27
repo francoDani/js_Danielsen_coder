a49fdd506ce969456f95cbc6010d4ccae40ddc99
Franco <franco.danielsen@gmail.com>
2022-05-26 23:27:19 -0300

    desafío complementario

--- index.js
+++ index.js
@@ -1,21 +1,32 @@
 let iteration = +prompt("ingresa un numero para ejecutar el bucle");
 
-for (let i = 1; i <= iteration; i++) {
-  console.log("esta es la iteración n°: " + i);
-}
+if(iteration != 0){
+  for (let i = 1; i <= iteration; i++) {
+    console.log("esta es la iteración n°: " + i);
+  }
+}else{console.log('cero no cuenta, perdiste')}
+
 
 let text = prompt("ingresa un texto para descomponer");
 
-for (let i = 0; i <= text.length; i++) {
+for (let i = 0; i < text.length; i++) {
   console.log(text[i]);
   if (i == text.length) {
     alert("tu texto se descompuso con éxito!");
   }
 }
 
-let esc = prompt("ingresa la palabra clave");
+let esc = (prompt("ingresa la palabra clave")).toUpperCase();
 
-while (esc != "esc") {
+
+while (esc != "ESC") {    
     console.log('Esa no es la palabra clave!');
     esc = prompt("no tuviste suerte!");
 };
+
+let number = +(prompt('ingresa un número'));
+
+for (let i = 0; i <= 10; i++) {
+  let result = number * i;
+  console.log(number + ' x ' + i + ' = '+ result);
+}
